2151acf33ad73207e46a915a141ab1aefcf7d0dc
binwu <binwu@asrmicro.com>
2017-12-11 15:32:13 +0800

    add  auto uart
    Change-Id: Ia1cd2e243020892fa18739de0757151094ec8990

---
+++ tool/auto_uart/uart_auto_input.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+from serial.tools.list_ports import comports
+import serial
+from serial import Serial
+import threading 
+import platform
+import sys,os,re
+import time
+import argparse
+import signal
+
+import commands
+
+stop_flag = threading.Event()
+def signal_handler(signum, frame):
+    print 'ctrl_c exit.' 
+    stop_flag.set()
+    sys.exit(0)
+
+class Uart(threading.Thread):
+	def __init__(self, log_file = None, is_print = True):
+		super(Uart,self).__init__()
+		if log_file:
+			self.log = open(log_file,'w')
+		else:
+			self.log = open('test_log.log','w')
+		self.comport = None
+		self.last_log = None
+		self.case_end_flag = False
+		self.result_log = []
+		self.is_print = is_print
+		self.last_log_bak = ''
+		self.timeout = 0.01
+
+	def input(self,msg):
+		if self.comport:
+			self.case_end_flag = False
+			self.last_log = None
+			self.result_log = []
+			self.comport.write(msg+'\n')
+			return 1
+		else:
+			print 'Pls create port'
+			return 0
+
+	def reset_log_file(self,file):
+		if self.log:
+			self.log.flush()
+			self.log.close()
+			self.log = None
+		self.log = open(file,'w')
+
+	def save_log_file(self):
+		if self.log:
+			self.log.flush()
+			self.log.close()
+			self.log = None
+		else:
+			print 'No log file'
+
+	def expect(self,text,timeout):
+		try:
+			cnt = int((timeout+self.timeout)/self.timeout)
+			for i in range(cnt):
+				# if self.last_log == text: return True
+				if self.last_log and text in self.last_log: return True
+				time.sleep(self.timeout)
+			return False
+		except Exception,e:
+			print e
+
+	def createPort(self, port = None, baud = 115200, time_out = 0.1):
+		if port:
+			pass
+		else:
+			platform_type = platform.platform()
+			port_list = comports()
+			if "Linux" in platform_type:
+				port_list = [port[0] for port in port_list]
+				port_list = [port for port in port_list if "ttyUSB" in port]
+			elif "Windows" in platform_type:
+				port_list = [list(port) for port in port_list]
+				port_list = [port for port in port_list if "USB to UART Bridge" in port[1]]
+			if port_list:
+				port = port_list[0]
+				os.system('sudo chmod 777 %s'%port)
+				print port
+			else:
+				print "No port"
+				sys.exit(0)
+		self.comport = Serial(port, baud, timeout=time_out)
+		self.timeout = time_out
+		if self.comport.isOpen():
+			return 1
+		else:
+			print "create port fail"
+			sys.exit(1)
+
+	def run(self):
+		while 1:
+			try:
+				if stop_flag.is_set(): break
+				line = self.comport.readline().strip()
+				self.last_log = line
+				if line == 'ctest#':
+					if line == self.last_log_bak:
+						sys.stdout.write('\n'+line+' ')
+						sys.stdout.flush()
+					else:
+						sys.stdout.write(line+' ')
+						sys.stdout.flush()
+				elif line and self.is_print:
+					print line
+				self.last_log_bak = line
+				if self.log and line:
+					print >>self.log,line
+					self.log.flush()
+				if line:
+					self.last_log = line
+					if 'AUTOTEST@' in self.last_log:
+						match = re.search(r'AUTOTEST@.*?Result\[(.*?)\]:RetCode(\[.*?\])',self.last_log)
+						if match:
+							if match.group(1) == 'OK':
+								self.result_log.append(match.group(1))
+							else:
+								self.result_log.append(match.group(1)+match.group(2))
+					if 'Unknown command' in self.last_log:
+						self.result_log.append(self.last_log.split('-')[0].strip())
+					if 'ctest#' == line.strip():
+						self.case_end_flag = True if self.result_log else False
+			except Exception,e:
+				print e,'Serial Exception.Pls check serial'
+				stop_flag.set()
+				break
+		self.comport.close()
+		if self.log:
+			self.log.flush()
+			self.log.close()
+
+
+class UartController(threading.Thread):
+	def __init__(self,uart):
+		super(UartController,self).__init__()
+		self.uart = uart
+
+	def exit_serial(self):
+		stop_flag.set()
+		sys.exit(0)
+
+	def parse_input(self,text):
+		if 'exit' in text: 
+			self.exit_serial()
+		if 'is_print' in text:
+			value = text.split('=')[1].strip()
+			if eval(value):
+				self.uart.is_print = True
+			else:
+				self.uart.is_print = False
+
+	def run(self):
+		try:
+			self.uart.start() if not self.uart.is_alive() else None
+			while 1:
+				msg = raw_input('')
+				self.parse_input(msg)
+				if stop_flag.is_set(): self.exit_serial()
+				self.uart.input(msg) if 'is_print' not in msg else None
+		except Exception,e:
+			print e,'Pls check serial port'
+			self.exit_serial()
+
+def WAIT_ALL_THREAD_END():
+	while 1:
+		alive = False
+		for th in threading.enumerate():
+			if th.getName() is 'MainThread': continue
+			alive = alive or th.isAlive()
+		if not alive: break
+
+if __name__ == "__main__":
+	signal.signal(signal.SIGINT, signal_handler)
+	signal.signal(signal.SIGTERM, signal_handler)
+	arg_parser = argparse.ArgumentParser()
+	arg_parser.add_argument('-f','--log_file',default = './test_log.log',help='log path or log_file -- example: ./test_log.log')
+	arg_parser.add_argument('-d','--output_dir',default = './',help='output file dir -- example: ./')
+	argv = arg_parser.parse_args()
+
+	uart = Uart(argv.log_file)
+	uart.createPort()
+	uart_manager = UartController(uart)
+	uart_manager.start()
+
+	try:
+		with open('./tool/auto_uart/autocmd.cfg') as file_obj:
+			file_text = '|'.join(file_obj.readlines())
+			file_text = '|' + re.sub('\n','',file_text)
+		cmd_set = re.findall('\|AUTOTEST@\((.*?),(.*?)\)',file_text)
+		for cmd,timeout in cmd_set:
+			uart.input(cmd.strip())
+			time.sleep(eval(timeout))
+		WAIT_ALL_THREAD_END()
+	except Exception,e:
+		stop_flag.set()
+		print e
+		sys.exit(1)
