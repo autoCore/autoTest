6284d80417990dcc361719ee82ad09be81e3d13c
binwu <binwu@asrmicro.com>
2018-05-28 18:13:55 +0800

    delete surplus files
    Change-Id: Ifced691a762b5aff61b6505de83f828002c0dc2b

--- tool/autoCode/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -f *.pyc
-rm -f *.bak
-rm -f *.~
-rm -f *~
-rm -rf __pycache__
-
--- tool/autoCode/xml_file.py
+++
@@ -1,7 +0,0 @@
-xml_file = [				\
-	"./tmp/pmuap.xml",		\
-	"./tmp/pmumain.xml",		\
-	"./tmp/ciu.xml",		\
-	"./tmp/rtc.xml", 		\
-	"./tmp/dma.xml", 		\
-]
