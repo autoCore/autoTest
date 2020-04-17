a8a4bb41f3b27f653839056d3306c7ac938eff85
robbinwu <binwu@asrmicro.com>
2020-04-17 18:27:50 +0800

    create myrepo py

--- tool/build_release_tool/build_release.py
+++ tool/build_release_tool/build_release.py
@@ -13,22 +13,11 @@ from release_zip import *
 from send_email import *
 from ftp import ftp_upload_file
 from TriggerTest import trigger_test
+from myrepo import *
+
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
 
-manifest_xml = \
-"""
-<?xml version="1.0" encoding="UTF-8"?>
-<manifest>
-  <remote fetch=".." name="origin" review="https://source.asrmicro.com/"/>
-  
-  <default remote="origin" revision="master" sync-j="4"/>
-  <project name="crane/crane-dev" path="crane" revision="%s" upstream="master"/>
-  <project name="crane/cp" path="crane/cp" revision="%s" upstream="master"/>
-  <project name="crane/gui" path="crane/gui" revision="%s" upstream="master"/>
-  <project name="crane/tool" path="crane/tool" revision="%s" upstream="master"/>
-</manifest>
-"""
 
 email_subject = "CRANE PHONE: AUTO RELEASE %s"
 email_msg = r"""
@@ -142,226 +131,6 @@ class buildController(object):
         # shutil.copytree(src,dist)
         logger.info("%s copy build done"%src)
 
-
-class myRepo(object):
-    def __init__(self, version_log, root_path = '',storage_list = ['','cp','gui','tool']):
-        self.root_path = root_path
-        self._storage_list = storage_list
-        self.CUR_DATE = 0
-        self.version_log = version_log
-        self.menifest_xml = ''
-        self.cp_version = None
-        self.dsp_version = None
-
-    def clone(self,clone_path):
-        for storage in self._storage_list:
-            _path = os.path.join(self.root_path, storage)
-            _repo = git.Repo(_path)
-            _path = os.path.join(clone_path, storage)
-            _repo.clone(_path)
-
-    def sync(self):
-        ret = None
-        self.git_clean()
-        for storage in self._storage_list:
-            _path = os.path.join(self.root_path, storage)
-            _repo = git.Repo(_path)
-            _git = _repo.git
-            _git.config("--global","core.autocrlf","false")
-            try:
-                info = _git.pull()
-                if not storage:
-                    storage = 'hal'
-                logger.debug("%-4s %s"%(storage,info))
-                if "Already up to date." not in info:
-                    ret = 1
-            except Exception,e:
-                logger.error(e)
-                ret = None
-        return ret
-
-    def git_clean(self):
-        for storage in self._storage_list:
-            _path = os.path.join(self.root_path, storage)
-            _repo = git.Repo(_path)
-            _git = _repo.git
-            try:
-                _git.clean("-xdf")
-                _git.reset("--hard","HEAD")
-            except Exception,e:
-                logger.error(e)
-
-
-    def get_revion_owner(self):
-        info_list = []
-        for storage in self._storage_list:
-            _path = os.path.join(self.root_path, storage)
-            _repo = git.Repo(_path)
-            _git = _repo.git
-            info = _git.log("-1","--date=raw",'--pretty=format:"<%H> <%ae> <%cd> <%s>"')
-            info = info.replace('\n','')
-            text_list = re.findall(r'<(.*?)> <(.*?)> <([0-9]+).*?> <(.*?)>',info)[0]
-            logger.debug(storage,text_list)
-            info_list.append(text_list)
-        # CUR_DATE_bak = self.CUR_DATE
-        for index, text_list in enumerate(info_list):
-            logger.debug(text_list)
-            commit_id, Author_email,date, commit_info = text_list
-            if int(date) >= self.CUR_DATE:
-                self.CUR_DATE = int(date)
-        # if CUR_DATE_bak == self.CUR_DATE:
-            # return None
-        for index, text_list in enumerate(info_list):
-            commit_id, Author_email, date, commit_info= text_list
-            if self.CUR_DATE == int(date):
-                return commit_id, Author_email,date, commit_info
-
-    def get_commit_massages(self,delta_time = 7):
-        now = datetime.datetime.today()
-        offset = datetime.timedelta(days = delta_time)
-        date = (now - offset).strftime("%Y-%m-%d")
-        massage_file = "release_notes.txt"
-        massage_file = os.path.join(self.root_path,massage_file)
-        info_list = []
-        for storage in self._storage_list:
-            if storage is 'tool':
-                continue
-            _path = os.path.join(self.root_path, storage)
-            _repo = git.Repo(_path)
-            _git = _repo.git
-            info = _git.log("--after=%s"%date,"--pretty=format:commit ID: %H\nAuthor: %an <%ae>\nDate:  %cd\ncommit msg: %s\n\
-------------------------------------------------------------")
-            info_list.append("*"*50)
-            if not storage:
-                storage = 'hal'
-            header = "%s commit info"%storage 
-            info_list.append(header)
-            info_list.append("*"*50)
-            info_list.append(info)
-        with open(massage_file,'w') as obj:
-            obj.write('\n'.join(info_list))
-        return massage_file
-
-    def get_revion_info(self):
-        info_list = []
-        for storage in self._storage_list:
-            _path = os.path.join(self.root_path, storage)
-            _repo = git.Repo(_path)
-            _git = _repo.git
-            info = _git.log("-1","--pretty=format:commit: %H\nAuthor: %cn <%ce>\nDate:  %cd\n------------------------------------------------")
-            info_list.append(info)
-        return "\n".join(info_list)
-
-    def get_manifest_xml(self,xml_file):
-        info_list = []
-        for storage in self._storage_list:
-            _path = os.path.join(self.root_path, storage)
-            _repo = git.Repo(_path)
-            _git = _repo.git
-            info = _git.log("-1","--pretty=format:%H")
-            logger.debug(storage,info)
-            info_list.append(info)
-        manifest_text = manifest_xml%tuple(info_list)
-        logger.debug(manifest_text)
-        _obj = open(xml_file,'w')
-        _obj.write(manifest_text.lstrip())
-        _obj.close()
-
-    def get_nearest_version(self):
-        flog = open(self.version_log)
-        text = flog.read()
-        text = text.replace('\n','')
-        text_list = re.findall(r'crane_git_r([0-9]+)',text)
-        logger.debug(text_list)
-        if text_list:
-            cnt = int(text_list[-1])
-        else:
-            cnt = 0
-        return "crane_git_r%d"%(cnt+1)
-
-    def record_version(self,version = '',info = ''):
-        flog = open(self.version_log,'a')
-        if not version:
-            version = self.get_nearest_version()
-        if not info:
-            info = self.get_revion_info()
-        text = "\n".join([version+":",info])
-        flog.write(text+"\n\n")
-        flog.flush()
-        flog.close()
-        return version
-
-    def get_cp_version(self,cp_version_file):
-        '''#define CRANE_CUST_VER_INFO 
-        "["##SYSTEM_VERSION##"]
-        ["##DISTRIBUTION_VERSION##"]
-        ["##SYSTEM_TARGET_OS##"]
-        ["##SYSTEM_PS_MODE##"]
-        ["##APPEND_REVERSION##"]"'''
-        assert os.path.exists(cp_version_file),"%s not exists"%cp_version_file
-        SYSTEM_CUST_SKU = "MINIGUI"
-        SYSTEM_SKU_REVERSION = "SDK"
-        SYSTEM_PS_MODE = "LTEGSM"
-        SYSTEM_TARGET_OS = "TX"
-        APPEND_REVERSION = "_".join([SYSTEM_CUST_SKU,SYSTEM_SKU_REVERSION])
-        file_obj = open(cp_version_file)
-        for _line in file_obj:
-            format = '#define[ ]+SYSTEM_VERSION[ ]+"(.*?)"'
-            match = re.findall(format,_line)
-            if match:
-                SYSTEM_VERSION = match[0]
-            format = '#define[ ]+DISTRIBUTION_VERSION[ ]+"(.*?)"'
-            match = re.findall(format,_line)
-            if match:
-                DISTRIBUTION_VERSION = match[0]
-        CRANE_CUST_VER_INFO = "[%s][%s][%s][%s][%s]"%(SYSTEM_VERSION,\
-        DISTRIBUTION_VERSION,SYSTEM_TARGET_OS,SYSTEM_PS_MODE,APPEND_REVERSION)
-        logger.debug(CRANE_CUST_VER_INFO)
-        self.cp_version = CRANE_CUST_VER_INFO
-        return CRANE_CUST_VER_INFO
-
-    def update_cp_version(self,cp_version_file,cp_version_log_file):
-        self.cp_version = self.get_cp_version(cp_version_file)
-        with open(cp_version_log_file,"w") as obj:
-            obj.write(self.cp_version)
-        return self.cp_version
-
-    def get_old_cp_version(self,cp_version_log_file):
-        if not os.path.exists(cp_version_log_file):
-            return None
-        else:
-            with open(cp_version_log_file) as obj:
-                return obj.read()
-
-    def record_cp_version(self,cp_version_log_file):
-        with open(cp_version_log_file,"w") as obj:
-            obj.write(self.cp_version)
-
-    def get_dsp_version(self,dsp_bin,dsp_version_log_file):
-        "!CRANE_CAT1GSM_L1_1.043.000 , Dec 13 2019 03:30:56"
-        fobj = open(dsp_bin,"rb")
-        text = fobj.read()
-        # match = re.findall("!(CRANE_.*?[0-9][0-9]:[0-9][0-9]:[0-9][0-9])",text)
-        match = re.findall("(CRANE_.{47})",text)
-        if match:
-            version_info = match[0]
-            logger.debug(version_info)
-            self.dsp_version = version_info
-        else:
-            version_info = "can not match dsp version".upper()
-        with open(dsp_version_log_file,"w") as obj:
-            obj.write(version_info)
-
-class cusRepo(myRepo):
-    def __init__(self, version_log, root_path):
-        super(cusRepo,self).__init__(version_log, root_path, ['.'])
-
-        _path = os.path.join(self.root_path, '.')
-        self.git = git.Repo(_path).git
-        if cfg.release_branch in "r1":
-            self.git.checkout("r1")
-        else:
-            self.git.checkout("master")
 
 
 class dailyBuild(object):
@@ -877,9 +646,9 @@ if __name__ == "__main__":
 
     logger.resetLogFile(log_file)
     logger.debug(cfg)
-    repo = myRepo(cfg.version_log, cfg.cur_crane)
+    repo = myRepo(logger, cfg.version_log, cfg.cur_crane)
     repo.update_cp_version(os.path.join(cfg.cur_crane,cfg.cp_version_file),cfg.cp_version_log)
-    repo_cus = cusRepo(cfg.version_cus_log,cfg.cur_crane_cus)
+    repo_cus = cusRepo(logger, cfg.version_cus_log,cfg.cur_crane_cus, cfg.release_branch)
 
     build_controller = buildController(cfg)
     download_controller = downloadToolController(cfg, logger)
