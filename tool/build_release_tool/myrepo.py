d99c992350e14a4f2fdca80e69ca7ae0058f9b47
binwu <binwu@asrmicro.com>
2020-05-18 15:59:13 +0800

    format py

--- tool/build_release_tool/myrepo.py
+++ tool/build_release_tool/myrepo.py
@@ -5,9 +5,8 @@ import datetime
 import re
 import git
 
-
 manifest_xml = \
-"""
+    """
 <?xml version="1.0" encoding="UTF-8"?>
 <manifest>
   <remote fetch=".." name="origin" review="https://source.asrmicro.com/"/>
@@ -22,7 +21,9 @@ manifest_xml = \
 
 
 class myRepo(object):
-    def __init__(self, logger, version_log, root_path = '',storage_list = ['','cp','gui','tool']):
+    def __init__(self, logger, version_log, root_path='', storage_list=None):
+        if storage_list is None:
+            storage_list = ['', 'cp', 'gui', 'tool']
         self.root_path = root_path
         self._storage_list = storage_list
         self.CUR_DATE = 0
@@ -36,8 +37,7 @@ class myRepo(object):
 
         self.get_verion_name()
 
-
-    def clone(self,clone_path):
+    def clone(self, clone_path):
         for storage in self._storage_list:
             _path = os.path.join(self.root_path, storage)
             _repo = git.Repo(_path)
@@ -51,7 +51,7 @@ class myRepo(object):
             _path = os.path.join(self.root_path, storage)
             _repo = git.Repo(_path)
             _git = _repo.git
-            _git.config("--global","core.autocrlf","false")
+            _git.config("--global", "core.autocrlf", "false")
             try:
                 info = _git.pull()
                 if not storage:
@@ -59,7 +59,7 @@ class myRepo(object):
                 # self.log.debug("%-4s %s"%(storage,info))
                 if "Already up to date." not in info:
                     ret = 1
-            except Exception,e:
+            except Exception, e:
                 self.log.error(e)
                 ret = None
         return ret
@@ -71,11 +71,10 @@ class myRepo(object):
             _git = _repo.git
             try:
                 _git.clean("-xdf")
-                _git.reset("--hard","HEAD")
-            except Exception,e:
+                _git.reset("--hard", "HEAD")
+            except Exception, e:
                 self.log.error(storage)
                 self.log.error(e)
-
 
     def get_revion_owner(self):
         info_list = []
@@ -83,30 +82,30 @@ class myRepo(object):
             _path = os.path.join(self.root_path, storage)
             _repo = git.Repo(_path)
             _git = _repo.git
-            info = _git.log("-1","--date=raw",'--pretty=format:"<%H> <%ae> <%cd> <%s>"')
-            info = info.replace('\n','')
-            text_list = re.findall(r'<(.*?)> <(.*?)> <([0-9]+).*?> <(.*?)>',info)[0]
-            self.log.debug(storage,text_list)
+            info = _git.log("-1", "--date=raw", '--pretty=format:"<%H> <%ae> <%cd> <%s>"')
+            info = info.replace('\n', '')
+            text_list = re.findall(r'<(.*?)> <(.*?)> <([0-9]+).*?> <(.*?)>', info)[0]
+            self.log.debug(storage, text_list)
             info_list.append(text_list)
         # CUR_DATE_bak = self.CUR_DATE
         for index, text_list in enumerate(info_list):
             self.log.debug(text_list)
-            commit_id, Author_email,date, commit_info = text_list
+            commit_id, Author_email, date, commit_info = text_list
             if int(date) >= self.CUR_DATE:
                 self.CUR_DATE = int(date)
         # if CUR_DATE_bak == self.CUR_DATE:
-            # return None
+        # return None
         for index, text_list in enumerate(info_list):
-            commit_id, Author_email, date, commit_info= text_list
+            commit_id, Author_email, date, commit_info = text_list
             if self.CUR_DATE == int(date):
-                return commit_id, Author_email,date, commit_info
+                return commit_id, Author_email, date, commit_info
 
-    def get_commit_massages(self,delta_time = 7):
+    def get_commit_massages(self, delta_time=7):
         now = datetime.datetime.today()
-        offset = datetime.timedelta(days = delta_time)
+        offset = datetime.timedelta(days=delta_time)
         date = (now - offset).strftime("%Y-%m-%d")
         massage_file = "release_notes.txt"
-        massage_file = os.path.join(self.root_path,massage_file)
+        massage_file = os.path.join(self.root_path, massage_file)
         info_list = []
         for storage in self._storage_list:
             if storage is 'tool':
@@ -114,16 +113,16 @@ class myRepo(object):
             _path = os.path.join(self.root_path, storage)
             _repo = git.Repo(_path)
             _git = _repo.git
-            info = _git.log("--after=%s"%date,"--pretty=format:commit ID: %H\nAuthor: %an <%ae>\nDate:  %cd\ncommit msg: %s\n\
+            info = _git.log("--after=%s" % date, "--pretty=format:commit ID: %H\nAuthor: %an <%ae>\nDate:  %cd\ncommit msg: %s\n\
 ------------------------------------------------------------")
-            info_list.append("*"*50)
+            info_list.append("*" * 50)
             if not storage:
                 storage = 'hal'
-            header = "%s commit info"%storage 
+            header = "%s commit info" % storage
             info_list.append(header)
-            info_list.append("*"*50)
+            info_list.append("*" * 50)
             info_list.append(info)
-        with open(massage_file,'w') as obj:
+        with open(massage_file, 'w') as obj:
             obj.write('\n'.join(info_list))
         return massage_file
 
@@ -133,29 +132,30 @@ class myRepo(object):
             _path = os.path.join(self.root_path, storage)
             _repo = git.Repo(_path)
             _git = _repo.git
-            info = _git.log("-1","--pretty=format:commit: %H\nAuthor: %cn <%ce>\nDate:  %cd\n------------------------------------------------")
+            info = _git.log("-1",
+                            "--pretty=format:commit: %H\nAuthor: %cn <%ce>\nDate:  %cd\n------------------------------------------------")
             info_list.append(info)
         return "\n".join(info_list)
 
-    def get_manifest_xml(self,xml_file):
+    def get_manifest_xml(self, xml_file):
         info_list = []
         for storage in self._storage_list:
             _path = os.path.join(self.root_path, storage)
             _repo = git.Repo(_path)
             _git = _repo.git
-            info = _git.log("-1","--pretty=format:%H")
+            info = _git.log("-1", "--pretty=format:%H")
             # self.log.debug(storage,info)
             info_list.append(info)
         manifest_text = manifest_xml.format(info_list)
-        self.log.info(manifest_text)
-        _obj = open(xml_file,'w')
+        self.log.debug(manifest_text)
+        _obj = open(xml_file, 'w')
         _obj.write(manifest_text.lstrip())
         _obj.close()
 
     def get_verion_name(self):
         flog = open(self.version_log)
         text = flog.read()
-        text = text.replace('\n','')
+        text = text.replace('\n', '')
         text_list = re.findall(self.version_pattern, text)
         self.log.debug(text_list)
         if text_list:
@@ -168,7 +168,7 @@ class myRepo(object):
     def get_nearest_version(self):
         flog = open(self.version_log)
         text = flog.read()
-        text = text.replace('\n','')
+        text = text.replace('\n', '')
         # text_list = re.findall(r'crane_git_r([0-9]+)',text)
         text_list = re.findall(self.version_pattern, text)
         self.log.debug(text_list)
@@ -176,90 +176,92 @@ class myRepo(object):
             # assert text_list,"can not find version info"
             self.verion_name, cnt = text_list[-1]
             cnt = int(cnt)
-            version = "%s%04d"%(self.verion_name,cnt+1)
+            version = "%s%04d" % (self.verion_name, cnt + 1)
             self.log.info(version)
         else:
             version = self.release_branch
         return version
 
-    def record_version(self,version = '',info = ''):
-        flog = open(self.version_log,'a')
+    def record_version(self, version='', info=''):
+        flog = open(self.version_log, 'a')
         if not version:
             version = self.get_nearest_version()
         if not info:
             info = self.get_revion_info()
-        text = "\n".join([version+":",info])
-        flog.write(text+"\n\n")
+        text = "\n".join([version + ":", info])
+        flog.write(text + "\n\n")
         flog.flush()
         flog.close()
         return version
 
-    def get_cp_version(self,cp_version_file):
+    def get_cp_version(self, cp_version_file):
         '''#define CRANE_CUST_VER_INFO 
         "["##SYSTEM_VERSION##"]
         ["##DISTRIBUTION_VERSION##"]
         ["##SYSTEM_TARGET_OS##"]
         ["##SYSTEM_PS_MODE##"]
         ["##APPEND_REVERSION##"]"'''
-        assert os.path.exists(cp_version_file),"%s not exists"%cp_version_file
+        assert os.path.exists(cp_version_file), "%s not exists" % cp_version_file
         SYSTEM_CUST_SKU = "MINIGUI"
         SYSTEM_SKU_REVERSION = "SDK"
         SYSTEM_PS_MODE = "LTEGSM"
         SYSTEM_TARGET_OS = "TX"
-        APPEND_REVERSION = "_".join([SYSTEM_CUST_SKU,SYSTEM_SKU_REVERSION])
+        APPEND_REVERSION = "_".join([SYSTEM_CUST_SKU, SYSTEM_SKU_REVERSION])
         file_obj = open(cp_version_file)
         SYSTEM_VERSION = ''
         DISTRIBUTION_VERSION = ''
         for _line in file_obj:
             format = '#define[ ]+SYSTEM_VERSION[ ]+"(.*?)"'
-            match = re.findall(format,_line)
+            match = re.findall(format, _line)
             if match:
                 SYSTEM_VERSION = match[0]
             format = '#define[ ]+DISTRIBUTION_VERSION[ ]+"(.*?)"'
-            match = re.findall(format,_line)
+            match = re.findall(format, _line)
             if match:
                 DISTRIBUTION_VERSION = match[0]
-        CRANE_CUST_VER_INFO = "[%s][%s][%s][%s][%s]"%(SYSTEM_VERSION,\
-        DISTRIBUTION_VERSION,SYSTEM_TARGET_OS,SYSTEM_PS_MODE,APPEND_REVERSION)
+        CRANE_CUST_VER_INFO = "[%s][%s][%s][%s][%s]" % (SYSTEM_VERSION, \
+                                                        DISTRIBUTION_VERSION, SYSTEM_TARGET_OS, SYSTEM_PS_MODE,
+                                                        APPEND_REVERSION)
         # self.log.debug(CRANE_CUST_VER_INFO)
         self.cp_version = CRANE_CUST_VER_INFO
         return CRANE_CUST_VER_INFO
 
-    def update_cp_version(self,cp_version_file,cp_version_log_file):
+    def update_cp_version(self, cp_version_file, cp_version_log_file):
         self.cp_version = self.get_cp_version(cp_version_file)
-        with open(cp_version_log_file,"w") as obj:
+        with open(cp_version_log_file, "w") as obj:
             obj.write(self.cp_version)
         return self.cp_version
 
-    def get_old_cp_version(self,cp_version_log_file):
+    def get_old_cp_version(self, cp_version_log_file):
         if not os.path.exists(cp_version_log_file):
             return None
         else:
             with open(cp_version_log_file) as obj:
                 return obj.read()
 
-    def record_cp_version(self,cp_version_log_file):
-        with open(cp_version_log_file,"w") as obj:
+    def record_cp_version(self, cp_version_log_file):
+        with open(cp_version_log_file, "w") as obj:
             obj.write(self.cp_version)
 
-    def get_dsp_version(self,dsp_bin,dsp_version_log_file):
+    def get_dsp_version(self, dsp_bin, dsp_version_log_file):
         "!CRANE_CAT1GSM_L1_1.043.000 , Dec 13 2019 03:30:56"
-        fobj = open(dsp_bin,"rb")
+        fobj = open(dsp_bin, "rb")
         text = fobj.read()
         # match = re.findall("!(CRANE_.*?[0-9][0-9]:[0-9][0-9]:[0-9][0-9])",text)
-        match = re.findall("(CRANE_.{47})",text)
+        match = re.findall("(CRANE_.{47})", text)
         if match:
             version_info = match[0]
             # self.log.debug(version_info)
             self.dsp_version = version_info
         else:
             version_info = "can not match dsp version".upper()
-        with open(dsp_version_log_file,"w") as obj:
+        with open(dsp_version_log_file, "w") as obj:
             obj.write(version_info)
+
 
 class cusRepo(myRepo):
     def __init__(self, logger, cfg):
-        super(cusRepo,self).__init__(logger, cfg.version_cus_log, cfg.cur_crane_cus, ['.'])
+        super(cusRepo, self).__init__(logger, cfg.version_cus_log, cfg.cur_crane_cus, ['.'])
         _path = os.path.join(self.root_path, '.')
         self.git = git.Repo(_path).git
         self.cfg = cfg
@@ -277,5 +279,3 @@ class cusRepo(myRepo):
             self.release_dist_dir = self.cfg.release_r1_tmp
 
         self.get_verion_name()
-
-
